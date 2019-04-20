3e4e7ef1bce17d77fa41c52f80f6e8663921f9c0
Jesus Sarmiento <jessar1996@outlook.com>
2019-04-20 16:39:11 -0500

    Outputting content

---
+++ resources/js/components/users/Index.vue
@@ -0,0 +1,48 @@
+<template>
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">User</div>
+
+                <div class="card-body">
+                    
+                    <user v-for="user in users" :user="user" :key="user.id"> </user>
+   
+                </div>
+            </div>
+        </div>
+    </div> 
+
+        
+    
+</template>
+
+<script>
+
+     import User from './partials/User.vue'
+
+
+export default {
+    
+    components: { 
+
+    User
+    
+    },
+  
+    data () {
+        return {
+            users: []
+        }
+    },
+
+    mounted() {
+      
+      axios.get('/api/users').then ((response)=> {
+          this.users = response.data.data;
+      })
+
+    },
+}
+</script>
---
+++ resources/js/components/users/partials/User.vue
@@ -0,0 +1,54 @@
+<template>
+
+   <div class="media">
+      
+       <div class="media-left m-bottom">
+
+           <a href="#"><img src="https://via.placeholder.com/60x60" :alt="user.name"></a>
+
+       </div>
+
+       <div class="media-body m-left">
+
+           <h5 class="media-heading">
+
+               {{ user. name }}
+
+           </h5>
+
+           {{ user.email}}
+
+       </div>
+       
+   </div>
+        
+
+</template>
+
+
+
+<script>
+export default {
+
+    props : ['user']
+    
+}
+
+</script>
+
+
+<style lang="scss">
+
+   .m-bottom {
+
+       margin-bottom:13px
+
+   }
+
+   .m-left {
+
+       margin-left:10px
+
+   }
+    
+</style>

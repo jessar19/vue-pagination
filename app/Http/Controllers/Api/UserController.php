4e0b423efb3a5bcbc1a7bd37b09a28b2bb0a3190
Jesus Sarmiento <jessar1996@outlook.com>
2019-04-20 15:34:27 -0500

    Test user endpoint

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+                        
+use App\User;
+use App\Http\Resources\UsersCollection;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return new UsersCollection(User::paginate(10));
+
+    }
+}
